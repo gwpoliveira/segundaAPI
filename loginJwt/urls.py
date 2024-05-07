ac3d577133d5865f0429da63324cfca8f24b64c9
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-05-07 20:08:02 -0300

    api
    coment

--- loginJwt/urls.py
+++ loginJwt/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from . import views
 
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
+    TokenRefreshView,
 )
 
 urlpatterns = [
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenObtainPairView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.RegisterView.as_view(), name='auth_register'),
     path('test/', views.testEndPoint, name='test'),
-    path('', views.getRoutes),
+    path('loginjwt/', views.getRoutes),
 
 ]
